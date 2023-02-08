f3b950d3f1786e48355907e21f3cc2b12ade3695
John H <jdhwang98@gmail.com>
2023-02-07 18:31:50 -0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,55 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  
+  private userInput : string = "";
 
+ getUserInput() : string {
+  let userInput = (<HTMLInputElement> document.getElementById("myTextInput")).value;
+    this.setExpression(userInput);
+    return userInput;
+  }
+
+  setExpression (word: string): void {
+    this.userInput = word;
+  }
+
+  changePicture(): void {
+    if (isPalindrome(this.userInput)) {
+      (<HTMLInputElement> document.getElementById("image")).src="https://static.tvtropes.org/pmwiki/pub/images/spongebob_chicken.png";
+    }
+    else {
+      (<HTMLInputElement> document.getElementById("image")).src="https://i.ytimg.com/vi/wAbnNZDhYrA/maxresdefault.jpg";
+    }
+  }
+}
+
+const isPalindrome = (userInput: string): boolean => {
+  let i = 0;
+  let j = userInput.length - 1;
+  let counter = 0; 
+  for (i < j; ++i ; --j) {
+    if (userInput[i] == userInput[j]) {
+      counter++;
+    }
+
+  }
+  if (counter = userInput.length) {
+    return true
+  }
+  else {
+    return false;
+  }
+  
+}
+
+
+const setboolExpression = (isPalindrome: boolean) => {
+  
+  if (isPalindrome == false) {
+    return 
+  }
+  else {
+    return
+  }
 }
